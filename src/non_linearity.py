d9b4c3e7c8448b7062b43009da6eae8bfed7da09
Vladimir <eremeevvv72@gmail.com>
2023-01-21 10:16:26 +0500

    implemented Relu

--- src/non_linearity.py
+++ src/non_linearity.py
@@ -1,7 +1,10 @@
 from src.layer import Layer
+
 import numpy as np
 
+
 class Sigmoid(Layer):
+    X: np.array
 
     def forward(self, X: np.array) -> np.array:
         self.X = X
@@ -13,3 +16,15 @@ class Sigmoid(Layer):
     @staticmethod
     def _sigmoid_fn(X: np.array) -> np.array:
         return 1 / (1 + np.e ** (-1 * X))
+
+
+class Relu(Layer):
+    X: np.array
+
+    def forward(self, X: np.array) -> np.array:
+        self.X = X
+        return np.maximum(0, X)
+
+    def backward(self, dz: np.array) -> np.array:
+        dz[self.X < 0] = 0
+        return dz
--- tests/test_non_linearity.py
+++ tests/test_non_linearity.py
@@ -8,14 +8,14 @@ def test_sigmoid_fn():
     result = sigmoid._sigmoid_fn(given)
     assert result.sum() <= 1.62246 and len(result) == 3
 
-def test_forward():
+def test_sigmoid_forward():
     sigmoid = non_linearity.Sigmoid()
     given = np.array([1,0.5,-1])
     sigmoid.forward(given)
     assert (sigmoid.X == given).all()
 
 
-def test_backward():
+def test_sigmoid_backward():
     sigmoid = non_linearity.Sigmoid()
     given = np.array([1,0.5,-1])
     dz = np.array([0.5])
@@ -23,3 +23,20 @@ def test_backward():
     expected = np.array([0.09830597, 0.11750186, 0.09830597])
     dX = sigmoid.backward(dz)
     assert len(dX) == 3 and (dX.round(5) == expected.round(5)).all()
+
+
+def test_relu_forward():
+    given_x  = np.array([0,0.5,10,-4,-0.4])
+    expected = np.array([0,0.5,10,0,0])
+    relu = non_linearity.Relu()
+    result = relu.forward(given_x)
+    assert np.array_equal(result, expected) and np.array_equal(relu.X, given_x)
+
+
+def test_relu_backward():
+    relu = non_linearity.Relu()
+    relu.X =  np.array([0,0.5,10,-4,-0.4])
+    given_dz = np.array([5,5,5,5,5])
+    expected = np.array([5,5,5,0,0])
+    result = relu.backward(given_dz)
+    assert np.array_equal(result,expected)
